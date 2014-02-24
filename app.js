a75bebd50a4790289116a155a809cfdad4197ef5
Ryan Rusnak <ryandrusnak@gmail.com>
2014-02-24 10:18:52 -0500

    adding heroku mongoose stuff

--- app.js
+++ app.js
@@ -6,13 +6,37 @@ var handler = function(req, res) {
 	});
 }
 
-// mongoose stuff
+// Pre- Heoku mongoose stuff
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
   // yay!
+});
+//Post heroku mongoose stuff
+var http = require ('http');             // For serving a basic web page.
+var mongoose = require ("mongoose"); // The reason for this demo.
+
+// Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/test';
+
+// The http server will listen to an appropriate port, or default to
+// port 5000.
+var theport = process.env.PORT || 5000;
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
 });
 
 var userSchema = mongoose.Schema({
